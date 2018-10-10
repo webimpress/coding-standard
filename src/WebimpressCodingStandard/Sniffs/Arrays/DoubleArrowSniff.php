de94105e0eea43a57f2e81a112a3a48408c67247
webimpress <contact@webimpress.com>
2018-10-10 10:40:02 +0100

    Fixed typo in PHPDocs

--- src/WebimpressCodingStandard/Sniffs/Arrays/DoubleArrowSniff.php
+++ src/WebimpressCodingStandard/Sniffs/Arrays/DoubleArrowSniff.php
@@ -17,7 +17,7 @@ use const T_WHITESPACE;
 class DoubleArrowSniff extends AbstractArraySniff
 {
     /**
-     * The maximum amount od f padding before the alignment is ignored.
+     * The maximum amount of padding before the alignment is ignored.
      *
      * If the amount of padding required to align this assignment with the
      * surrounding assignments exceeds this number, the assignment will be

72a03a2c8bbc0e8749ca5509ba1c14f36a149e15
webimpress <contact@webimpress.com>
2018-11-27 11:17:08 +0000

    Do not require space before second line of comment

--- src/WebimpressCodingStandard/Sniffs/Commenting/PlacementSniff.php
+++ src/WebimpressCodingStandard/Sniffs/Commenting/PlacementSniff.php
@@ -30,7 +30,10 @@ class PlacementSniff implements Sniff
     {
         $tokens = $phpcsFile->getTokens();
 
-        if ($tokens[$stackPtr - 1]['code'] !== T_WHITESPACE) {
+        if ($tokens[$stackPtr - 1]['code'] !== T_WHITESPACE
+            && ($tokens[$stackPtr - 1]['code'] !== T_COMMENT
+                || $tokens[$stackPtr - 1]['line'] === $tokens[$stackPtr]['line'])
+        ) {
             $error = 'Expected at least one space before comment';
             $fix = $phpcsFile->addFixableError($error, $stackPtr, 'MissingSpaceBefore');
 

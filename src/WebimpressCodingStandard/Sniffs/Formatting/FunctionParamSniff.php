ee20f56596e002f7e3184ea8a7ec9d01ff7f3818
webimpress <contact@webimpress.com>
2018-11-29 20:49:27 +0000

    Added new sniff Formatting.FunctionParam
    Always one space after param typehint and param name

---
+++ src/WebimpressCodingStandard/Sniffs/Formatting/FunctionParamSniff.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandard\Sniffs\Formatting;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Util\Tokens;
+
+use const T_BITWISE_AND;
+use const T_CLOSURE;
+use const T_FUNCTION;
+use const T_WHITESPACE;
+
+class FunctionParamSniff implements Sniff
+{
+    /**
+     * @return int[]
+     */
+    public function register() : array
+    {
+        return [T_CLOSURE, T_FUNCTION];
+    }
+
+    /**
+     * @param int $stackPtr
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $token = $tokens[$stackPtr];
+
+        // Skip broken function declarations.
+        if (! isset($token['scope_opener'], $token['parenthesis_opener'])) {
+            return;
+        }
+
+        $params = $phpcsFile->getMethodParameters($stackPtr);
+        foreach ($params as $param) {
+            if (! $param['type_hint']) {
+                continue;
+            }
+
+            $last = $phpcsFile->findPrevious(
+                Tokens::$emptyTokens + [T_BITWISE_AND => T_BITWISE_AND],
+                $param['token'] - 1,
+                null,
+                true
+            );
+
+            if ($tokens[$last + 1]['content'] !== ' ') {
+                $error = 'Expected one space after param type hint';
+                $fix = $phpcsFile->addFixableError($error, $last + 1, 'SpaceAfterTypeHint');
+
+                if ($fix) {
+                    $phpcsFile->fixer->beginChangeset();
+                    if ($tokens[$last + 1]['code'] === T_WHITESPACE) {
+                        $token = $last + 1;
+                        $phpcsFile->fixer->replaceToken($token, ' ');
+                        while ($tokens[++$token]['code'] === T_WHITESPACE) {
+                            $phpcsFile->fixer->replaceToken($token, '');
+                        }
+                    } else {
+                        $phpcsFile->fixer->addContent($last, ' ');
+                    }
+                    $phpcsFile->fixer->endChangeset();
+                }
+            }
+        }
+    }
+}
---
+++ test/Sniffs/Formatting/FunctionParamUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandardTest\Sniffs\Formatting;
+
+use WebimpressCodingStandardTest\Sniffs\AbstractTestCase;
+
+class FunctionParamUnitTest extends AbstractTestCase
+{
+    protected function getErrorList(string $testFile = '') : array
+    {
+        return [
+            4 => 1,
+            5 => 1,
+            6 => 1,
+            7 => 1,
+            8 => 1,
+            13 => 2,
+            15 => 1,
+        ];
+    }
+
+    protected function getWarningList(string $testFile = '') : array
+    {
+        return [];
+    }
+}

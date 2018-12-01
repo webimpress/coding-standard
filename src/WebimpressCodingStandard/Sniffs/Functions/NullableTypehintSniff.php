8c91cb1c2439e90315fe65dfba843f67f9c5db83
webimpress <contact@webimpress.com>
2018-12-01 10:57:04 +0000

    Added new sniff Functions.NullableTypehint
    Requires or disallow question mark for parameters with default null value
    (by default we require them)

---
+++ src/WebimpressCodingStandard/Sniffs/Functions/NullableTypehintSniff.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandard\Sniffs\Functions;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+use function strtolower;
+
+use const T_CLOSURE;
+use const T_FUNCTION;
+use const T_NULLABLE;
+
+class NullableTypehintSniff implements Sniff
+{
+    /**
+     * @var bool
+     */
+    public $withQuestionMark = true;
+
+    /**
+     * @return int[]
+     */
+    public function register() : array
+    {
+        return [T_FUNCTION, T_CLOSURE];
+    }
+
+    /**
+     * @param int $stackPtr
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $params = $phpcsFile->getMethodParameters($stackPtr);
+
+        foreach ($params as $param) {
+            if (empty($param['type_hint_token'])) {
+                continue;
+            }
+
+            if (empty($param['default']) || strtolower($param['default']) !== 'null') {
+                continue;
+            }
+
+            if ($this->withQuestionMark && ! $param['nullable_type']) {
+                $error = 'Question mark is required with nullable typehints';
+                $fix = $phpcsFile->addFixableError($error, $param['type_hint_token'], 'MissingQuestionMark');
+
+                if ($fix) {
+                    $phpcsFile->fixer->addContentBefore($param['type_hint_token'], '?');
+                }
+            } elseif (! $this->withQuestionMark && $param['nullable_type']) {
+                $questionMark = $phpcsFile->findPrevious(T_NULLABLE, $param['type_hint_token']);
+
+                $error = 'Question mark is redundant for parameters with default null value';
+                $fix = $phpcsFile->addFixableError($error, $questionMark, 'RedundantQuestionMark');
+
+                if ($fix) {
+                    $phpcsFile->fixer->replaceToken($questionMark, '');
+                }
+            }
+        }
+    }
+}
---
+++ test/Sniffs/Functions/NullableTypehintUnitTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandardTest\Sniffs\Functions;
+
+use WebimpressCodingStandardTest\Sniffs\AbstractTestCase;
+
+class NullableTypehintUnitTest extends AbstractTestCase
+{
+    protected function getErrorList(string $testFile = '') : array
+    {
+        switch ($testFile) {
+            case 'NullableTypehintUnitTest.1.inc':
+                return [
+                    7 => 1,
+                    17 => 1,
+                ];
+        }
+
+        return [
+            6 => 1,
+            9 => 1,
+            16 => 1,
+            19 => 1,
+        ];
+    }
+
+    protected function getWarningList(string $testFile = '') : array
+    {
+        return [];
+    }
+}

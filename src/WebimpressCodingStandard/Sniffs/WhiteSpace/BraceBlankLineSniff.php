dc676e8f635150f569ebfc8a7b95598a9892a008
webimpress <contact@webimpress.com>
2018-12-01 13:09:38 +0000

    Renamed sniff WhiteSpace.NoBlankLineAtStart to WhiteSpace.BraceBlankLine
    The sniff checks now if there is no any empty line after opening
    brace and before closing brace. It check all braces, so also these from
    anonymous classes, classes, traits, interfaces, functions, closures,
    control structures, loops, try...catch...finally, ...
    
    It replaces couple of other rules - now excluded in ruleset.xml

---
+++ src/WebimpressCodingStandard/Sniffs/WhiteSpace/BraceBlankLineSniff.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandard\Sniffs\WhiteSpace;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+use const T_OPEN_CURLY_BRACKET;
+use const T_WHITESPACE;
+
+class BraceBlankLineSniff implements Sniff
+{
+    /**
+     * @return int[]
+     */
+    public function register() : array
+    {
+        return [T_OPEN_CURLY_BRACKET];
+    }
+
+    /**
+     * @param int $stackPtr
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $next = $phpcsFile->findNext(T_WHITESPACE, $stackPtr + 1, null, true);
+        if ($tokens[$next]['line'] > $tokens[$stackPtr]['line'] + 1) {
+            $error = 'Blank line found after opening brace';
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'SpacingAfterOpen');
+
+            if ($fix) {
+                $phpcsFile->fixer->beginChangeset();
+                $i = $stackPtr + 1;
+                while ($tokens[$i]['line'] + 1 < $tokens[$next]['line']) {
+                    $phpcsFile->fixer->replaceToken($i, '');
+                    ++$i;
+                }
+                $phpcsFile->fixer->endChangeset();
+            }
+        }
+
+        $closer = $tokens[$stackPtr]['bracket_closer'];
+        $prev = $phpcsFile->findPrevious(T_WHITESPACE, $closer - 1, null, true);
+        if ($tokens[$closer]['line'] > $tokens[$prev]['line'] + 1) {
+            $error = 'Blank line found before closing brace';
+            $fix = $phpcsFile->addFixableError($error, $closer, 'SpacingBeforeClose');
+
+            if ($fix) {
+                $phpcsFile->fixer->beginChangeset();
+                $i = $prev + 1;
+                while ($tokens[$i]['line'] + 1 < $tokens[$closer]['line']) {
+                    $phpcsFile->fixer->replaceToken($i, '');
+                    ++$i;
+                }
+                $phpcsFile->fixer->endChangeset();
+            }
+        }
+    }
+}
---
+++ test/Sniffs/WhiteSpace/BraceBlankLineUnitTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebimpressCodingStandardTest\Sniffs\WhiteSpace;
+
+use WebimpressCodingStandardTest\Sniffs\AbstractTestCase;
+
+class BraceBlankLineUnitTest extends AbstractTestCase
+{
+    protected function getErrorList(string $testFile = '') : array
+    {
+        return [
+            3 => 1,
+            7 => 1,
+            10 => 1,
+            14 => 1,
+            19 => 1,
+            21 => 1,
+            25 => 1,
+            29 => 1,
+            34 => 1,
+            38 => 1,
+            41 => 1,
+            45 => 1,
+            48 => 1,
+            51 => 1,
+            53 => 1,
+            57 => 1,
+            59 => 1,
+            62 => 1,
+            68 => 1,
+            72 => 1,
+            74 => 1,
+            76 => 1,
+            78 => 1,
+            80 => 1,
+            84 => 1,
+            86 => 1,
+            88 => 1,
+            90 => 1,
+            92 => 1,
+            96 => 1,
+            100 => 2,
+            104 => 1,
+            106 => 1,
+            108 => 1,
+            113 => 1,
+            115 => 1,
+            119 => 2,
+            123 => 2,
+            127 => 1,
+        ];
+    }
+
+    protected function getWarningList(string $testFile = '') : array
+    {
+        return [];
+    }
+}
